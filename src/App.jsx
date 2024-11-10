1a9721df33b744b08842cdaf347fe2de8f2d3f9c
Jaywant Chaudhary <147032549+jaywant67@users.noreply.github.com>
2024-11-10 14:34:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Navigation from './components/navigation';
+import HeroSection from './components/Hero';
+
+const App = () => {
+return (
+ <div>
+    <Navigation />
+    <HeroSection />
+  </div>
+);
+};
+
+export default App;
